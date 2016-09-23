4fc9a7e3f9940cc0dad8bbdc48a4e7516d644448
Soreasan <soreasan@gmail.com>
2016-09-23 08:20:12 -0600

    Added a function about Break and continue from pages 64-65 in the K&R book.  Unfortunately I can't tell if it's working or not but my code does match the book.

---
+++ TrimWhiteSpace.c
@@ -0,0 +1,36 @@
+/*
+Kenneth Adair
+K&R page 65
+The trim function is supposed to remove trailing blanks, tabs, and newlines.  i attemped to create my own test method but I can't tell if it's working.  Nevertheless I must move on to the next section.  =(
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int trim(char s[]);
+int test();
+
+int main()
+{
+  int pokemon = test();
+  return 0;
+}
+
+/* trim: remove trailing blanks, tabs, newlines */
+int trim(char s[])
+{
+  int n;
+  for(n = strlen(s)-1; n>= 0; n--)
+	if(s[n] != ' ' && s[n] != '\t' && s[n] != '\n')
+	  break;
+  /*s[n+1] = '\0';*/
+      return n;
+}
+
+int test(){
+  char s[10] = " a a a   ";
+  printf("%s\n", s);
+  s = trim(s);
+  printf("%s\n", s);
+  return 0;
+}
