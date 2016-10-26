a4334c8b0b61c50791deb6921ee238cd530ae2ab
Soreasan <soreasan@gmail.com>
2016-10-26 11:09:25 -0600

    Created a program that swaps two values using pointers based on pg 96 of the K&R book

---
+++ SwapWithPointers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int *px, int *py);
+
+int main(){
+  int a, b;
+  a = 5;
+  b = 7;
+  printf("Before the swap function:\n a = %d\n b = %d\n", a, b);
+  swap(&a, &b);
+  printf("After the swap function:\n a = %d\n b = %d\n", a, b);
+}
+
+/* Interchange *px and *py  */
+void swap(int *px, int *py)
+{
+  int temp;
+  temp = *px;
+  *px = *py;
+  *py = temp;
+}
