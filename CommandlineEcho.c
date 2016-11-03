1c410d17f2c7ec6e4619cc65fa3e0d776a252f2e
Kenneth Adair <SOREASAN@GMAIL.COM>
2016-11-03 10:03:11 -0600

    Completed the 2nd example on pg. 115 of K&R

--- CommandlineEcho.c
+++ CommandlineEcho.c
@@ -6,12 +6,12 @@ K&R pg. 115
 #include <stdio.h>
 
 /* echo command-line arguments; 1st edition */
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for(i = 1; i < argc; i++)
-		printf("%s%s", argv[i], (i < argc-1) ? " ": "";
+		printf("%s%s", argv[i], (i < argc-1) ? " ": "");
 	printf("\n");
 	return 0;
 }
---
+++ CommandlineEcho2.c
@@ -0,0 +1,15 @@
+/*
+Kenneth Adair
+K&R pg. 115
+*/
+
+#include <stdio.h>
+
+/* echo command-line arguments; 2nd version */
+int main(int argc, char *argv[])
+{
+	while(--argc > 0)
+		printf("%s%s", *++argv, (argc > 1) ? " " : "");
+	printf("\n");
+	return 0;
+}
